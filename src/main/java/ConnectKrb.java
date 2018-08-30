6a1d176a34780c59c381a788bec78ff4bc61936b
Alex Bleasdale <ableasdale@marklogic.com>
2018-08-30 12:22:22 +0100

    Everything so far

---
+++ src/main/java/ConnectKrb.java
@@ -0,0 +1,24 @@
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+
+public class ConnectKrb {
+
+    //
+    // private DatabaseClient client;
+    private static String appServerHostName = "engrlab-130-217";
+    private static String kdcPrincipalUser = "mjones@kerberostest.local";
+    private static int appServerHostPort = 9001;
+    private static Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    public static void main(String[] args) {
+
+        LOG.info("trying to connect using the Kerberos Auth Context");
+        DatabaseClient client = DatabaseClientFactory.newClient(appServerHostName,
+                appServerHostPort, new DatabaseClientFactory.KerberosAuthContext(kdcPrincipalUser));
+        client.newServerEval().xquery("1+1");
+    }
+}
