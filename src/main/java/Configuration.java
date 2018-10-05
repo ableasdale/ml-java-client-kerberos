22ce739b1b859fcb866326133f448704d35bd3bc
Alex Bleasdale <ableasdale@marklogic.com>
2018-10-05 12:13:22 +0100

    Checking in Configuration file

---
+++ src/main/java/Configuration.java
@@ -0,0 +1,5 @@
+public class Configuration {
+    public static String MARKLOGIC_HOST = "marklogic_hostname";
+    public static String KDC_PRINCIPAL_USER = "kcd_principal_user";
+    public static int APPSERVER_PORT = 0000;
+}
